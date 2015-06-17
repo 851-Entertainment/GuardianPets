9eba678e33798ac8461eea0c4c56a6c16d1045a2
AHBrandon <brandoncoulthard@hotmail.com>
2015-06-17 10:36:04 -0400

    Pet Love Var

--- Assets/Scripts/Pet.cs
+++ Assets/Scripts/Pet.cs
@@ -11,6 +11,7 @@ public class Pet : MonoBehaviour
     public int m_Hunger; //The pet's hunger level -- 100 is max, 0 is min. Max is starving, min is full. Max = bad, Min = good.
     public int m_Cleanliness; //The pet's cleanliness level -- 100 is max, 0 is min. Max is filthy, min is spotless. Max = bad, Min = good.
     public int m_Bored; //The pet's bored level -- 100 is max, 0 is min. Max is extremely bored, min is entertained. Max = bad, Min = good.
+    public int m_Love;
     public string m_FearOne; //The fear the player chose when they picked the pet
     public string m_FearTwo; //This fear is only used if the player pays for a second fear slot
 
@@ -39,6 +40,7 @@ public class Pet : MonoBehaviour
         UpdateAnim();
         UpdateSpeechBubble();
         SpeechBubbleTimer();
+        m_Love = m_Hunger + m_Cleanliness + m_Bored;
 	}
 
     void UpdateAnim()
