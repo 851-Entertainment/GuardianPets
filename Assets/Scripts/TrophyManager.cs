5a351c71afda68b7c6af47d2be57cf495df58a38
AHBrandon <brandoncoulthard@hotmail.com>
2015-07-06 12:13:42 -0400

    More trophy functionality
    After 2 seconds the trophy will go back to its default state.

--- Assets/Scripts/TrophyManager.cs
+++ Assets/Scripts/TrophyManager.cs
@@ -13,13 +13,23 @@ public class TrophyManager : MonoBehaviour
     private Color startColor;
     private Color highlighColor_;
     private Button button_;
-    private bool firstClick_ = true; 
+    private bool firstClick_ = true;
+    private bool updateTimer_ = false;
+    private float timer_ = 0.0f;
 
     void Start()
     {
         button_ = GetComponent<Button>();
         startColor = Color.white;
         highlighColor_ = Color.red;
+    }
+
+    void Update()
+    {
+        if(updateTimer_)
+        {
+            DisableTrophy(2.0f);
+        }
     }
 
     public void Highlight()
@@ -29,12 +39,26 @@ public class TrophyManager : MonoBehaviour
             firstClick_ = !firstClick_;
             button_.image.color = highlighColor_;
             m_TextField.text = m_Description;
+            updateTimer_ = true;
         }
         else
         {
             firstClick_ = !firstClick_;
             button_.image.color = startColor;
             m_TextField.text = "";
+        }
+    }
+
+    /// <param name="maxTime">After a set time the trophy will go back to its default state</param>
+    void DisableTrophy(float maxTime)
+    {
+        timer_ += Time.deltaTime;
+        if (timer_ >= maxTime)
+        {
+            firstClick_ = false;
+            Highlight();
+            updateTimer_ = false;
+            timer_ = 0.0f;
         }
     }
 }
