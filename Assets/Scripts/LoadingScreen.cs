f44d36a007067b601e995dcdb219e14fe9deb2d3
Andrew Best <andrew.c.best@gmail.com>
2015-07-14 14:41:36 -0400

    New Loading Screen
    Not deleting the Main Menu yet until we know we don't need it, but it's
    about to be useless

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Soomla.Store;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Text m_Text;
+    public GameObject m_ProgressBar;
+
+    private int loadProgress_ = 0;
+    private float timer_ = 0.0f;
+
+    void Start()
+    {
+        SoomlaStore.Initialize(new GuardianPetsAssets());
+        SoomlaStore.StartIabServiceInBg();
+        SoomlaStore.StopIabServiceInBg();
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+
+	void Update () 
+    {
+        StartCoroutine(DisplayLoadingScreen());
+	}
+
+    IEnumerator DisplayLoadingScreen()
+    {
+        m_ProgressBar.transform.localScale = new Vector3(loadProgress_, m_ProgressBar.transform.localScale.y, m_ProgressBar.transform.localScale.z);
+
+        m_Text.text = "Loading..." + loadProgress_ + "%";
+
+        AsyncOperation async = Application.LoadLevelAsync("Game");
+
+        while(!async.isDone)
+        {
+            loadProgress_ = (int)(async.progress * 100);
+            m_Text.text = "Loading..." + loadProgress_ + "%";
+            m_ProgressBar.transform.localScale = new Vector3(async.progress, m_ProgressBar.transform.localScale.y, m_ProgressBar.transform.localScale.z);
+            yield return null;
+        }
+    }
+}
