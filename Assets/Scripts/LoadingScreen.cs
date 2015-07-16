ef48e9973425aad2c0686cf9f5551dcd6e57c3a4
Andrew Best <andrew.c.best@gmail.com>
2015-07-16 13:39:35 -0400

    Loading screen stuff

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -11,6 +11,7 @@ public class LoadingScreen : MonoBehaviour
     private int loadProgress_ = 0;
     private float timer_ = 0.0f;
     private bool loadDone_ = false;
+    AsyncOperation async = null;
 
     void Start()
     {
@@ -18,11 +19,26 @@ public class LoadingScreen : MonoBehaviour
         SoomlaStore.StartIabServiceInBg();
         SoomlaStore.StopIabServiceInBg();
         Screen.orientation = ScreenOrientation.Landscape;
+        async = Application.LoadLevelAsync("Game");
     }
 
 	void Update () 
     {
         StartCoroutine(DisplayLoadingScreen());
+
+        if(async != null)
+        {
+            Debug.Log("async not null");
+            if(async.progress >= 0.9f)
+            {
+                Debug.Log("Async is done");
+                timer_ += Time.deltaTime;
+                if(timer_ >= 3.0f)
+                {
+                    async.allowSceneActivation = true;
+                }
+            }
+        }
 	}
 
     IEnumerator DisplayLoadingScreen()
@@ -31,16 +47,27 @@ public class LoadingScreen : MonoBehaviour
 
         m_Text.text = "Loading..." + loadProgress_ + "%";
 
-        AsyncOperation async = Application.LoadLevelAsync("Game");
+        
         async.allowSceneActivation = false;
 
         while (!async.isDone)
         {
+            Debug.Log("Loading");
             loadProgress_ = (int)(async.progress * 100);
             m_Text.text = "Loading..." + loadProgress_ + "%";
             m_ProgressBar.transform.localScale = new Vector3(async.progress, m_ProgressBar.transform.localScale.y, m_ProgressBar.transform.localScale.z);
-            
             yield return null;
+        }
+        //StartScene(async);
+        
+        
+    }
+
+    void StartScene(AsyncOperation scene)
+    {
+        if (scene.isDone)
+        {
+            scene.allowSceneActivation = true;
         }
     }
 }
